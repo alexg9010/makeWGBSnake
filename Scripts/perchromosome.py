47f7546a1403735c9fa04116f3030f2435ba0f94
Katarzyna Wreczycka <katwre@gmail.com>
2018-05-14 10:57:46 +0200

    Doesnt work on bih cluster

---
+++ Scripts/perchromosome.py
@@ -0,0 +1,156 @@
+############################## PER CHROMOSOME
+rule
+export_bigwig:
+input:
+seqlengths = os.path.join(DIR_mapped, "Refgen_" + ASSEMBLY + "_chromlengths.csv"),
+rdsfile = [os.path.join(DIR_methcall, "{sample}/{sample}_sorted_dedup_" + chrom + "_methylRaw.RDS") for chrom in CHROMS]
+output:
+bw = [os.path.join(DIR_bigwig, "{sample}_" + chrom + ".bw") for chrom in CHROMS]
+message: "Exporting bigwig files"
+shell:
+"""
+ {tools}/Rscript {DIR_scripts}/export_bw.R \
+                 {input.rdsfile} \
+                 {input.seqlengths} \
+                 ASSEMBLY \
+                 {output} \
+ """
+
+# I dont use it currently
+rule
+filterSNPs:
+input:
+bam = expand(DIR_methcall + "{{sample}}/{{sample}}_sorted_dedup_{chrom}_methylRaw.RDS", chrom=CHROMS),
+bed = "SNPFile.bed",
+tabixfile = expand(DIR_methcall + "{{sample}}/Tabix/{{sample}}_{chrom}.txt.bgz", chrom=CHROMS)
+output:
+expand(DIR_methcall + "{{sample}}/{{sample}}.deduped_{chrom}_filteredSNP.RDS", chrom=CHROMS)
+message: "Filtering SNPs"
+shell:
+"{tools}/Rscript {DIR_scripts}/FilterSNP.R {input.bam} {input.bed} {output}"
+
+# I dont use it currently
+rule
+makeTabix:
+input:
+rdsfiles = [DIR_methcall + "{sample}/{sample}_sorted_dedup_" + chrom + "_methylRaw.RDS" for chrom in CHROMS],
+txtfiles = [DIR_methcall + "{sample}/{sample}_sorted_dedup_" + chrom + "_CpG.txt" for chrom in CHROMS]
+output:
+tabixdir = DIR_methcall + "{sample}/Tabix/",
+output = DIR_methcall + "{sample}/Tabix/{sample}_methyl.txt.bgz"
+# conda:
+# "Envs/env_min.yaml"
+shell:
+"""
+{tools}/Rscript {DIR_scripts}/makeTabix.R {output} {input}
+"""
+
+
+def getTreatment(samplename):
+    SAMPLE_TREAT_DICT[samplename]
+
+
+# rule merge_methCall:
+#     input:
+#        expand(DIR_methcall+"{{sample}}/{{sample}}_sorted_dedup_{chrom}_methylRaw.RDS", chrom=CHROMS)
+#     output:
+#        DIR_methcall+"{sample}/{sample}_sorted_dedup_methylRaw.RDS"
+#     params:
+#        treatment = lambda wildcards: SAMPLE_TREAT_DICT[wildcards.sample],
+#        mincov = MINCOV,
+#     shell:
+#        "{tools}/Rscript {DIR_scripts}/methCall_merge.R {output} {params.treatment} {params.mincov} {input}"
+
+
+rule
+bam_methCall_per_chr:
+input:
+bamfile = expand(DIR_bam_per_chrom + '{{sample}}/{{sample}}_sorted_dedup_{chrom}.bam', chrom=CHROMS)
+output:
+rdsfile = expand(DIR_methcall + "{{sample}}/{{sample}}_sorted_dedup_{chrom}_methylRaw.RDS", chrom=CHROMS),
+callFile = expand(DIR_methcall + "{{sample}}/{{sample}}_sorted_dedup_{chrom}_CpG.txt", chrom=CHROMS)
+params:
+assembly = ASSEMBLY,
+mincov = MINCOV,
+minqual = MINQUAL,
+context = "CpG",
+# savedb      = False #TODO,
+savefolder = DIR_methcall + "{sample}/"
+log:
+os.path.join(DIR_methcall, "{sample}/{sample}.deduped_meth_calls.log")
+message: "Extract methylation calls from bam file."
+shell:
+"""
+{tools}/Rscript {DIR_scripts}/methCall.R \
+        --inBam={input.bamfile} \
+        --assembly={params.assembly} \
+        --mincov={params.mincov} \
+        --minqual={params.minqual} \
+        --rds={output.rdsfile} \
+        --logFile={log}
+"""
+
+rule
+bam_sort_index_per_chr:
+input:
+expand(DIR_bam_per_chrom + '{{sample}}/{{sample}}_dedup_{chrom}.bam', chrom=CHROMS)
+output:
+expand(DIR_bam_per_chrom + '{{sample}}/{{sample}}_sorted_dedup_{chrom}.bam', chrom=CHROMS)
+params:
+sort_args = config['args']['sambamba_sort'],
+tmpdir = DIR_bam_per_chrom + "{sample}/"
+shell:
+"{tools}/sambamba sort {input} --tmpdir={params.tmpdir} -o {output} {params.sort_args}"
+
+# https://github.com/daler/enhancer-snakemake-demo/blob/master/Snakefile
+rule
+split_bam_per_chr:
+input:
+DIR_deduped + "{sample}/{sample}_sorted_dedup.bam"
+output:
+expand(DIR_bam_per_chrom + '{{sample}}/{{sample}}_dedup_{chrom}.bam', chrom=CHROMS)
+run:
+sampleid = list(wildcards)[0]
+for chrom in CHROMS:
+    cmd = tools + "/sambamba slice " + input[
+        0] + " " + chrom + " -o " + DIR_bam_per_chrom + sampleid + "/" + sampleid + "_dedup_" + chrom + ".bam"
+    # print(cmd)
+    shell(cmd)
+
+##########################################################
+
+
+#def input_bismark_align_unmapped_pe_as_se(wildcards):
+#    if (wildcards.ext=="1"):
+#       input=DIR_mapped+"{sample}/{sample}_unmapped_1.fq.gz"
+#    elif (wildcards.ext=="2"):
+#       input=DIR_mapped+"{sample}/{sample}_unmapped_2.fq.gz"
+#    return(input)
+
+#rule bismark_align_unmapped_pe_as_se:
+#    input:
+#        DIR_mapped+"{sample}/{sample}_unmapped_2.fq.gz"
+#    output:
+#        out = DIR_mapped+"{sample}/{sample}_unmapped_{ext}.bam",
+#        odir = DIR_mapped+"{sample}/"
+#    params:
+#        # intermediate files
+#        inter_bam = DIR_mapped+"{sample}_unmapped_2_bismark_bt2.bam",
+#        # bismark parameters
+#        bismark_args = config['args']['bismark_unmapped'],
+#        genomeFolder = "--genome_folder " + genomedir,
+#        outdir = "--output_dir  "+DIR_mapped+"{sample}/",
+#        pathToBowtie = "--path_to_bowtie " + config['tools'],
+#        useBowtie2  = "--bowtie2 ",
+#        samtools    = "--samtools_path "+ config['tools']+"samtools",
+#        tempdir     = "--temp_dir "+DIR_mapped+"{sample}/",
+#    log:
+#        DIR_mapped+"{sample}/{sample}_bismark_pe_mapping_unmapped_reads_2.log"
+#    message: "Mapping paired-end reads as single-end to genome."
+#    run:
+#        cmds=[
+#        '{tools}/bismark {params} {input.fin2} > {log} 2> {log}.err'
+#        'ln -s ' + output.odir + os.path.basename(input.fin2)+'_bismark_bt2_pe.bam'+ + ' {output.out2}'
+#        ]
+#        for c in cmds:
+#           shell(c)
