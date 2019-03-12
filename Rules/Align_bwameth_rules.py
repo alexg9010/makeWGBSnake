984248ee66284ad25a88442f999be2e247438010
Wreczycka <kwreczy@sl-akali-p-cs1.mdc-berlin.net>
2019-03-12 21:03:17 +0100

    temp2

--- Rules/Align_bwameth_rules.py
+++ Rules/Align_bwameth_rules.py
@@ -58,8 +58,36 @@ rule bwameth2bam_sort:
          "samtools view -Sb {input} > {output}; rm {input}"
 
 
-#if SUBSET_READS:
-# rule bwameth_align_pe_raw:
+if not SUBSET_READS:
+  
+  rule bwameth_align_pe_raw:
+     input:
+         genomefile+".bwameth.c2t.sa",
+         genomefile+".bwameth.c2t.amb",
+         genomefile+".bwameth.c2t.ann",
+         genomefile+".bwameth.c2t.pac",
+         genomefile+".bwameth.c2t.bwt",
+         genomefile+".bwameth.c2t",
+         fin1 = DIR_trimmed+"{sample}/{sample}_1_val_1.fq.gz",
+         fin2 = DIR_trimmed+"{sample}/{sample}_2_val_2.fq.gz"
+     output:
+         bam = DIR_mapped+"{sample}/{sample}.bwameth.sam"
+     params:
+        # bwa-meth parameters
+         bwameth_args = config['args']['bwameth']
+     log:
+         DIR_mapped+"{sample}/{sample}_bwameth_pe_mapping.log"
+     message: "Mapping paired-end reads to genome using bwa-meth."
+     shell:
+         "bwameth.py {params.bwameth_args} --reference {genomefile} {input.fin1} {input.fin2} > {output}"
+
+
+#########################################################################
+### SUBSET
+#########################################################################
+
+# if SUBSET_READS:
+#   rule bwameth_align_pe_raw:
 #      input:
 #          genomefile+".bwameth.c2t.sa",
 #          genomefile+".bwameth.c2t.amb",
@@ -79,6 +107,7 @@ rule bwameth2bam_sort:
 #      message: "Mapping paired-end reads to genome using bwa-meth."
 #      shell:
 #          "bwameth.py {params.bwameth_args} --reference {genomefile} {input.fin1} {input.fin2} > {output}"
+
 
 #if SUBSET_READS:
 # rule bwameth_align_pe:
